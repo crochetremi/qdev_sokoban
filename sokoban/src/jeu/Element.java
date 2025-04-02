f9583e2fd6948eca5c6394885df346fb4186b5f1
Rémi Crochet <remicrochet.310@orange.fr>
2025-04-02 17:19:34 +0200

    Ajout des attributs XY, constructeur et méthodes associées

--- sokoban/src/jeu/Element.java
+++ sokoban/src/jeu/Element.java
@@ -4,4 +4,63 @@ package jeu;
  * Classe décrivant les objets du jeu
  */
 public class Element {
+
+    /**
+     * Coordonné x d'un elt
+     */
+    private int x;
+
+    /**
+     * Coordonné y d'un elt
+     */
+    private int y;
+
+    /**
+     * Constructeur d'un elt
+     * @param x positif
+     * @param y positif
+     */
+    public Element(int x, int y) {
+        if( x > 0 && y > 0){
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    /**
+     * Getter
+     * @return x
+     */
+    public int getX() {
+        return x;
+    }
+
+    /**
+     * Getter
+     * @return y
+     */
+    public int getY() {
+        return y;
+    }
+
+    /**
+     * Setter
+     * @param x nouvelle coordonnée positive
+     */
+    public void setX(int x) {
+        if( x > 0 ) {
+            this.x = x;
+        }
+    }
+
+    /**
+     * Setter
+     * @param y nouvelle coordonnée positive
+     */
+    public void setY(int y) {
+        if( y > 0 ) {
+            this.y = y;
+        }
+    }
+
 }
